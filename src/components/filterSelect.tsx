823a3043eee4b1b5d39de26b347e4611c1fa3663
Elliot Saha <elliotsaha@gmail.com>
2023-12-28 00:22:16 -0700

    feat(filterselect): created filter select multiselect component

---
+++ src/components/filterSelect.tsx
@@ -0,0 +1,70 @@
+import {useState} from 'react';
+import {InputActionMeta, Select} from 'chakra-react-select';
+import {
+  Controller,
+  UseFormGetValues,
+  UseFormSetValue,
+  Control,
+  Path,
+} from 'react-hook-form';
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+type OptionFieldValues = {
+  [x: string]: Array<SelectOption>;
+};
+
+interface FilterSelectProps<T extends OptionFieldValues> {
+  getValues: UseFormGetValues<T>;
+  setValue: UseFormSetValue<T>;
+  control: Control<T>;
+  name: Path<T>;
+  options: SelectOption[];
+}
+
+function FilterSelect<T extends OptionFieldValues>({
+  control,
+  name,
+  options,
+}: FilterSelectProps<T>) {
+  const [selectVal, setSelectVal] = useState<string>();
+
+  const inputChangeHandler = (val: string, e: InputActionMeta) => {
+    switch (e.action) {
+      case 'set-value':
+        setSelectVal('');
+        break;
+      case 'input-change':
+        setSelectVal(val);
+        break;
+    }
+  };
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({field: {onChange, value, name, ref}}) => (
+        <Select
+          inputValue={selectVal}
+          onInputChange={inputChangeHandler}
+          placeholder={`${value.length} filter${
+            value.length === 1 ? '' : 's'
+          } selected`}
+          options={options}
+          controlShouldRenderValue={false}
+          isMulti
+          onChange={onChange}
+          value={value}
+          name={name}
+          ref={ref}
+        />
+      )}
+    />
+  );
+}
+
+export default FilterSelect;
