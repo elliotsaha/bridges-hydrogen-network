d709bdbc8f8558e70308d025e8beefede235f39e
Elliot Saha <elliotsaha@gmail.com>
2023-12-30 02:04:41 -0700

    feat(company/query): added validation to query API route, closes #60

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -32,7 +32,7 @@ import {useEffect, useState, ChangeEvent, useReducer} from 'react';
 import {Select} from 'chakra-react-select';
 import React from 'react';
 import FilterSelect from '@components/filterSelect';
-import {SelectOption} from '@types';
+import {SelectOption, SearchCompanyRequestFilters} from '@types';
 import {Company} from '@models';
 import axios from 'axios';
 
@@ -46,17 +46,8 @@ interface FormOptions {
   market_focus: SelectOption[];
   services: SelectOption[];
   technologies: SelectOption[];
-  types_of_business: SelectOption[];
+  type_of_business: SelectOption[];
   years_in_business?: SelectOption;
-}
-
-interface BodyRequest {
-  operating_regions: string[];
-  market_focus: string[];
-  services: string[];
-  technologies: string[];
-  types_of_business: string[];
-  years_in_business?: string;
 }
 
 const mapOptions = (options: SelectOption[]) => {
@@ -68,7 +59,7 @@ const defaultValues = {
   market_focus: [],
   technologies: [],
   operating_regions: [],
-  types_of_business: [],
+  type_of_business: [],
   services: [],
 };
 
@@ -76,7 +67,8 @@ const Search = () => {
   // rawSearchInput and requestBody must be seperated because
   // the searchbar input must be debounced
   const [rawSearchInput, setRawSearchInput] = useState<string>('');
-  const [requestBody, setRequestBody] = useState<BodyRequest>(defaultValues);
+  const [requestBody, setRequestBody] =
+    useState<SearchCompanyRequestFilters>(defaultValues);
 
   const debouncedSearchInput = useDebounce<string>(rawSearchInput, 1000);
 
@@ -165,7 +157,7 @@ const Search = () => {
       operating_regions,
       services,
       technologies,
-      types_of_business,
+      type_of_business,
       years_in_business,
     } = data;
 
@@ -174,7 +166,7 @@ const Search = () => {
       operating_regions: mapOptions(operating_regions),
       services: mapOptions(services),
       technologies: mapOptions(technologies),
-      types_of_business: mapOptions(types_of_business),
+      type_of_business: mapOptions(type_of_business),
       years_in_business: years_in_business?.value,
     };
 
@@ -191,7 +183,7 @@ const Search = () => {
           market_focus: requestBody.market_focus,
           services: requestBody.services,
           technologies: requestBody.technologies,
-          type_of_business: requestBody.types_of_business,
+          type_of_business: requestBody.type_of_business,
           years_in_business: requestBody.years_in_business,
         }
       );
@@ -310,7 +302,7 @@ const Search = () => {
                             <FilterSelect
                               size="md"
                               control={control}
-                              name="types_of_business"
+                              name="type_of_business"
                               options={selectOptions.businesses}
                             />
                           </Box>
@@ -342,41 +334,8 @@ const Search = () => {
                                 <Select
                                   size="md"
                                   selectedOptionStyle="check"
-                                  options={[
-                                    {
-                                      label: 'Less than 2 years',
-                                      value: JSON.stringify({
-                                        max: 2,
-                                      }),
-                                    },
-                                    {
-                                      label: '2-5',
-                                      value: JSON.stringify({
-                                        min: 2,
-                                        max: 5,
-                                      }),
-                                    },
-                                    {
-                                      label: '5-10',
-                                      value: JSON.stringify({
-                                        min: 5,
-                                        max: 10,
-                                      }),
-                                    },
-                                    {
-                                      label: '10-25',
-                                      value: JSON.stringify({
-                                        min: 10,
-                                        max: 25,
-                                      }),
-                                    },
-                                    {
-                                      label: '25+',
-                                      value: JSON.stringify({
-                                        min: 25,
-                                      }),
-                                    },
-                                  ]}
+                                  isClearable
+                                  options={yearsInBusinessFormOptions}
                                   {...field}
                                 />
                               </Box>
@@ -422,5 +381,41 @@ const Search = () => {
     </>
   );
 };
+
+const yearsInBusinessFormOptions = [
+  {
+    label: 'Less than 2 years',
+    value: JSON.stringify({
+      max: 2,
+    }),
+  },
+  {
+    label: '2-5',
+    value: JSON.stringify({
+      min: 2,
+      max: 5,
+    }),
+  },
+  {
+    label: '5-10',
+    value: JSON.stringify({
+      min: 5,
+      max: 10,
+    }),
+  },
+  {
+    label: '10-25',
+    value: JSON.stringify({
+      min: 10,
+      max: 25,
+    }),
+  },
+  {
+    label: '25+',
+    value: JSON.stringify({
+      min: 25,
+    }),
+  },
+];
 
 export default Search;
--- src/app/api/company/query/route.ts
+++ src/app/api/company/query/route.ts
@@ -3,143 +3,136 @@ import {connectToDatabase} from '@lib/mongoose';
 import {ServerResponse} from '@helpers/serverResponse';
 import {Company} from '@models/Company';
 import {PipelineStage} from 'mongoose';
+import {SearchCompanyRequest} from '@types';
+import z from 'zod';
 
 interface YearsData {
   min?: number;
   max?: number;
 }
 
-type CompanyType = {
-  company_name: string;
-  market_focus: string[];
-  services: string[];
-  technologies: string[];
-  type_of_business: string[];
-  operating_regions: string[];
-  years_in_business: string | undefined;
-};
+// Filters that follow the schema of Array<{ name: string, description?: string }>
+type NameFilterKeys = keyof Omit<
+  SearchCompanyRequest,
+  'company_name' | 'years_in_business' | 'operating_regions'
+>;
+
+const searchSchema = z.object({
+  company_name: z.string(),
+  operating_regions: z.string().array().optional(),
+  market_focus: z.string().array().optional(),
+  services: z.string().array().optional(),
+  technologies: z.string().array().optional(),
+  years_in_business: z.string().optional(),
+});
 
 export const POST = async (request: NextRequest) => {
-  try {
-    await connectToDatabase();
-    const body: CompanyType = await request.json();
+  await connectToDatabase();
 
-    const aggregation: PipelineStage[] = [];
+  const body: SearchCompanyRequest = await request.json();
 
-    let yearQuery = {};
+  const validation = searchSchema.safeParse(body);
 
-    if (body.years_in_business) {
-      const yearField: YearsData = JSON.parse(body.years_in_business);
-      console.log(yearField);
-      if (yearField.min === 25) {
-        yearQuery = {
-          $gte: 25,
-        };
-        aggregation.push({
-          $match: {
-            years_in_business: yearQuery,
-          },
-        });
-      } else if (yearField.max === 2) {
-        aggregation.push({
-          $match: {
-            less_than_2_years: true,
-          },
-        });
-      } else {
-        yearQuery = {
-          $gte: yearField.min,
-          $lte: yearField.max,
-        };
-        aggregation.push({
-          $match: {
-            years_in_business: yearQuery,
+  if (validation.success) {
+    try {
+      const aggregation: PipelineStage[] = [];
+
+      const arrayAggregator = (key: NameFilterKeys) => {
+        if (body[key].length !== 0) {
+          aggregation.push({
+            $match: {
+              [key]: {
+                $elemMatch: {
+                  name: {$in: body[key]},
+                },
+              },
+            },
+          });
+        }
+      };
+
+      // seach query needs to be before matching queries
+      if (body.company_name !== '') {
+        aggregation.unshift({
+          $search: {
+            autocomplete: {
+              fuzzy: {maxEdits: 1, maxExpansions: 256},
+              query: body.company_name,
+              path: 'company_name',
+            },
           },
         });
       }
-    }
 
-    if (body.company_name !== '') {
-      aggregation.unshift({
-        $search: {
-          autocomplete: {
-            fuzzy: {maxEdits: 1, maxExpansions: 256},
-            query: body.company_name,
-            path: 'company_name',
-          },
-        },
-      });
-    }
+      const YEARS_UPPER_LIMIT = 25;
+      const YEARS_LOWER_LIMIT = 2;
 
-    if (body.market_focus.length !== 0) {
-      aggregation.push({
-        $match: {
-          market_focus: {
-            $elemMatch: {
-              name: {$in: body.market_focus},
+      // Handles query for years_in_business & if less_than_2_years
+      if (body.years_in_business) {
+        const yearField: YearsData = JSON.parse(body.years_in_business);
+
+        if (yearField.min === YEARS_UPPER_LIMIT) {
+          aggregation.push({
+            $match: {
+              years_in_business: {
+                $gte: YEARS_UPPER_LIMIT,
+              },
+            },
+          });
+        } else if (yearField.max === YEARS_LOWER_LIMIT) {
+          aggregation.push({
+            $match: {
+              less_than_2_years: true,
+            },
+          });
+        } else {
+          aggregation.push({
+            $match: {
+              years_in_business: {
+                $gte: yearField.min,
+                $lte: yearField.max,
+              },
+            },
+          });
+        }
+      }
+
+      // operating regions need to be done seperately from rest
+      // of array filters because it doesn't have { name, description } schema
+      if (body.operating_regions.length !== 0) {
+        aggregation.push({
+          $match: {
+            operating_regions: {
+              $elemMatch: {$in: body.operating_regions},
             },
           },
-        },
-      });
+        });
+      }
+
+      const arrayFilters: NameFilterKeys[] = [
+        'market_focus',
+        'services',
+        'technologies',
+        'type_of_business',
+      ];
+
+      arrayFilters.map((i: NameFilterKeys) => arrayAggregator(i));
+
+      let res;
+
+      if (aggregation.length !== 0) {
+        res = await Company.aggregate(aggregation);
+      } else {
+        // if empty aggregation, return all companies
+        res = await Company.find();
+      }
+
+      return ServerResponse.success(res);
+    } catch (e) {
+      console.log(e);
+      return ServerResponse.serverError();
     }
-
-    if (body.services.length !== 0) {
-      aggregation.push({
-        $match: {
-          services: {
-            $elemMatch: {
-              name: {$in: body.services},
-            },
-          },
-        },
-      });
-    }
-
-    if (body.technologies.length !== 0) {
-      aggregation.push({
-        $match: {
-          technologies: {
-            $elemMatch: {
-              name: {$in: body.technologies},
-            },
-          },
-        },
-      });
-    }
-
-    if (body.type_of_business.length !== 0) {
-      aggregation.push({
-        $match: {
-          type_of_business: {
-            $elemMatch: {
-              name: {$in: body.type_of_business},
-            },
-          },
-        },
-      });
-    }
-
-    if (body.operating_regions.length !== 0) {
-      aggregation.push({
-        $match: {
-          operating_regions: {
-            $elemMatch: {$in: body.operating_regions},
-          },
-        },
-      });
-    }
-
-    let res;
-
-    if (aggregation.length !== 0) {
-      res = await Company.aggregate(aggregation);
-    } else {
-      res = await Company.find();
-    }
-
-    return ServerResponse.success(res);
-  } catch (e) {
-    console.log(e);
-    return ServerResponse.serverError('An error occured');
+  } else {
+    return ServerResponse.validationError(validation);
   }
 };
